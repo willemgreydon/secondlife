b261ca574e15fe10cd8e996d8eff55d96b025b48
willemgreydon <claus.nisslmueller@gmail.com>
2025-11-18 15:16:01 +0100

    Fixes

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -2,6 +2,7 @@
 'use client'
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity-studio/sanity.config'
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
